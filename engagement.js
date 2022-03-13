cff020d44c3990531c215fe720b97a0602642a7d
vireen2124 <24vireen@gmail.com>
2022-03-13 15:34:41 +0000

    test it out

--- engagement.js
+++ engagement.js
@@ -77,7 +77,7 @@ const countdown=()=>{
     const hoursTag=timeTags[1];
     const minutesTag=timeTags[2];
     const secondsTag=timeTags[3];
-    const engagementDate=new Date("march 12, 2022 15:00:00").getTime();
+    const engagementDate=new Date("march 26, 2022 15:00:00").getTime();
     //today date
     const today=new Date().getTime();
     //duration
